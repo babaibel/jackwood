f3ce31f38937ea1799aebf4b63de22d2eec47ecc
Babai <31babaev@gmail.com>
2017-08-25 18:03:29 +0300

    first commit

---
+++ mail.php
@@ -0,0 +1,19 @@
+<?
+if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){ //Проверка отправилось ли наше поля name и не пустые ли они
+        $to = 'prasolova20@gmail.com'; //Почта получателя, через запятую можно указать сколько угодно адресов
+        $subject = 'Обратный звонок'; //Заголовок сообщения
+        $message = '
+                <html>
+                    <head>
+                        <title>'.$subject.'</title>
+                    </head>
+                    <body>
+                        <p>Имя: <b>'.$_POST['name'].'</b></p>
+                        <p>Телефон: <b>'.$_POST['phone'].'</b></p>                     
+                    </body>
+                </html>'; //Текст нащего сообщения можно использовать HTML теги
+        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
+        $headers .= "From: Jackwood <prasolova20@gmail.com>\r\n"; //Наименование и почта отправителя
+        mail($to, $subject, $message, $headers); //Отправка письма с помощью функции mail
+}
+?>
